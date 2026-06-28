db6b3ec3ab1dd4c866c7ecb597f4fd20bcdda480
gunjan kumar <ss222802@gmail.com>
2026-06-28 12:40:55 +0530

    fix 404 error during refresh on user page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles/global.css';
@@ -10,14 +10,14 @@ import { TOAST_OPTIONS } from './utils/constants';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ToastContainer {...TOAST_OPTIONS} />
       <Routes>
         <Route path="/"          element={<Dashboard />}   />
         <Route path="/users/:id" element={<UserDetails />} />
         <Route path="*"          element={<NotFound />}    />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
